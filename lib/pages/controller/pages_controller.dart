321f806cef082fb46aa4a0ed88ec420d76cd0bed
park28s <park28s@naver.com>
2022-08-03 12:55:41 +0900

    mainPage 이번달목표 작업완료

--- lib/pages/controller/pages_controller.dart
+++ lib/pages/controller/pages_controller.dart
@@ -1,21 +1,5 @@
-import 'package:diabetes_life/main.dart';
-import 'package:diabetes_life/pages/model/check_model.dart';
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 
-class PagesController extends GetxController{
+class PagesController extends GetxController {
   static PagesController get to => Get.find();
-
-  int? diaTest;
-
-  final checkModel1 =
-  Hive.box<CheckModel>('check').get('${timeNow.year}-${timeNow.month}')?.diabetesEmptyStomach;
-
-  void checkTest() {
-    diaTest = checkModel1;
-    update();
-    print('diaTest == ${checkModel1}');
-  }
-
 }
